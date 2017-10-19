eec19c4eb5107d44cc54f7295f844ca385f149a4
Bjoerns-TB <30447739+Bjoerns-TB@users.noreply.github.com>
2017-10-19 14:32:27 +0200

    Create decoder.js

---
+++ ttn/decoder.js
@@ -0,0 +1,16 @@
+function Decoder(b, port) {
+
+  var lat = (b[0] | b[1]<<8 | b[2]<<16 | (b[2] & 0x80 ? 0xFF<<24 : 0)) / 10000;
+  var lon = (b[3] | b[4]<<8 | b[5]<<16 | (b[5] & 0x80 ? 0xFF<<24 : 0)) / 10000;
+  var alt = (b[6] | b[7]<<8 | (b[7] & 0x80 ? 0xFF<<16 : 0)) / 100;
+  var hdop = b[8] / 100;
+
+  return {
+    location: {
+      lat: lat,
+      lon: lon,
+      alt: alt,
+      hdop: hdop
+    },
+  };
+}
